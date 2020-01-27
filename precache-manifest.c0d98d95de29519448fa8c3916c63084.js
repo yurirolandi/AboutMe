00eec27e465f81771ce2fae273edebe92202d7a1
Yuri Rolandi <sd.rolandi@gmail.com>
2020-01-26 23:35:07 -0300

    Updates

---
+++ precache-manifest.c0d98d95de29519448fa8c3916c63084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c4f90843e8d2e93e5643520cede1e8",
+    "url": "/AboutMe/index.html"
+  },
+  {
+    "revision": "0030925bfa42c1a3469e",
+    "url": "/AboutMe/static/js/2.f58e5c3d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/AboutMe/static/js/2.f58e5c3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa573b20af56b9e30902",
+    "url": "/AboutMe/static/js/main.17d82fec.chunk.js"
+  },
+  {
+    "revision": "b8f20c8f95b795704470",
+    "url": "/AboutMe/static/js/runtime-main.a68d3746.js"
+  },
+  {
+    "revision": "58d058c580d5ad27b2ea7ed564501cba",
+    "url": "/AboutMe/static/media/perfil.58d058c5.jpeg"
+  }
+]);
